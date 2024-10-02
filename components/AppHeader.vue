d06e62586cf7c4a9eece2e19a8523e1713ad2bd2
jacopo-derrico <derricoj.design@gmail.com>
2024-10-02 17:34:20 +0200

    feat: add header and logo component

---
+++ components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="flex justify-between items-center my-8 mx-auto py-10 lg:py-0 lg:w-10/12">
+        <span class="primary-text">
+            _lang
+        </span>
+        <AppLogo />
+        <span class="accent-text">
+            _about
+        </span>
+    </header>
+</template>

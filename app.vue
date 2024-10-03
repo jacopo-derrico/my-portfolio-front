0b067fb92dbe4bbfc9a3423eb8dd31b2f07ef3e0
jacopo-derrico <derricoj.design@gmail.com>
2024-10-03 12:15:30 +0200

    feat: add hero section

--- app.vue
+++ app.vue
@@ -2,5 +2,6 @@
   <div>
     <NuxtRouteAnnouncer />
     <AppHeader />
+    <HomeHero />
   </div>
 </template>
---
+++ components/HomeHero.vue
@@ -0,0 +1,17 @@
+<template>
+    <section id="about-section" class="flex mx-auto py-10 my-[200px] lg:py-0 lg:w-10/12">
+        <div class="lg:w-1/2">
+            <h3 class="text-primary">Jacopo D'Errico</h3>
+            <h1 class="text-primary">Designer & Web Developer</h1>
+            <p class="text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis mi placerat, elementum purus et, fermentum odio. Duis vitae nisi iaculis ante bibendum convallis in et massa. Quisque id mauris sit amet dui sagittis feugiat. Nulla ut lorem eros. Sed congue nisl lacus, quis tempus dolor condimentum at. Sed maximus, velit id suscipit suscipit, dui turpis pharetra eros, ultricies aliquam enim mi id eros. </p>
+            <div class="flex justify-end mt-10">
+                <a href="">
+                    <button class="text-button uppercase btn-style py-3 px-5">_see projects</button>
+                </a>
+            </div>
+        </div>
+        <div class="lg:w-1/2">
+            <img src="" alt="">
+        </div>
+    </section>
+</template>

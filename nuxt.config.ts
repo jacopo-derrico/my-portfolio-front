2647fd1252aab2034b2cb785132e9f74f220b44d
jacopo-derrico <derricoj.design@gmail.com>
2024-10-03 11:43:10 +0200

    build: install nuxt-fonts package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts'],
   css: [
     '@/assets/main.scss'
   ],
@@ -15,4 +15,7 @@ export default defineNuxtConfig({
       },
     },
   },
+  fonts: {
+    local: {},
+  }
 })

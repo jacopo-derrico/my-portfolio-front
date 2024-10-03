98ecc58f30e5f371ab93433bfad3dd097260c812
jacopo-derrico <derricoj.design@gmail.com>
2024-10-03 12:15:57 +0200

    fix: change classes name

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,10 +1,10 @@
 <template>
     <header class="flex justify-between items-center my-8 mx-auto py-10 lg:py-0 lg:w-10/12">
-        <span class="primary-text">
+        <span class="text-primary">
             _lang
         </span>
         <AppLogo />
-        <span class="accent-text">
+        <span class="text-accent">
             _about
         </span>
     </header>

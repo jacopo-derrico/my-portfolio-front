7d829160bbdc0ba41ba4731823546976f46d2670
jacopo-derrico <derricoj.design@gmail.com>
2024-10-04 12:07:56 +0200

    build: install pinia store package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts', '@pinia/nuxt'],
   css: [
     '@/assets/main.scss'
   ],

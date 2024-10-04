ee166c590131e357c0d9f7753bf0e7fc39fd29c9
jacopo-derrico <derricoj.design@gmail.com>
2024-10-04 16:51:03 +0200

    refactor: change structure to implemente routing and pages

--- app.vue
+++ app.vue
@@ -2,7 +2,6 @@
   <div>
     <NuxtRouteAnnouncer />
     <AppHeader />
-    <HomeHero />
-    <HomeExperience />
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <HomeHero />
+        <HomeExperience />
+    </div>
+</template>

4cae4780a7e39dd9016eba44c00150c205d19389
jacopo-derrico <derricoj.design@gmail.com>
2024-10-08 16:36:29 +0200

    feat: masonry portfolio grid from api

---
+++ components/HomePortfolio.vue
@@ -0,0 +1,39 @@
+<template>
+    <section id="portfolio-section" class="mx-auto py-10 my-[200px] lg:py-0 lg:w-10/12">
+        <h3 class="text-button mb-10">// portfolio</h3>
+        <div class="columns-2 md:columns-4 gap-4 space-y-4">
+            <div v-for="project in portfolioStore.projects" :key="project.id" class="relative hover-info">
+                <img class="w-full rounded-xl shadow"
+                    :src="`${portfolioStore.backUrl}/storage/${project.cover_path}`" alt="">
+                <div class="absolute bottom-0 left-0 top-0 right-0 bg-black bg-opacity-80 flex flex-col-reverse p-4 hidden-info">
+                    <div class="flex gap-2 flex-wrap">
+                        <span v-for="(tag, index) in project.technologies" class="technology pill-type">
+                            {{tag.name}}
+                        </span>
+                    </div>
+                    <h6 class="mb-2 text-primary">
+                        _{{project.title}}
+                    </h6>
+                    <div class="flex gap-2 mb-auto">
+                        <span v-for="(cat, index) in project.categories" class="category pill-type">
+                            {{cat.name}}
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script setup>
+    import { usePortfolioStore } from '~/store/portfolio';
+
+    const portfolioStore = usePortfolioStore();
+
+    try {
+        portfolioStore.fetchProjects();
+    } catch (error) {
+        console.error('Error fetching projects:', error);
+    }
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,6 @@
     <div>
         <HomeHero />
         <HomeExperience />
+        <HomePortfolio />
     </div>
 </template>

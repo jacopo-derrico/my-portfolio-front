6cf3164dacacd06759c28300fb6f1ee4c0fdecd9
jacopo-derrico <derricoj.design@gmail.com>
2024-10-09 15:01:45 +0200

    feat: store for misc variables and function

---
+++ store/misc.ts
@@ -0,0 +1,11 @@
+import { Title } from '#build/components';
+import { defineStore } from 'pinia';
+
+export const useMiscStore = defineStore('miscStore', () => {
+    let myEmail = 'derricoj.design@gmail.com';
+    let gitHub = 'https://github.com/jacopo-derrico';
+    let insta = 'https://www.instagram.com/deja_unleashed/profilecard/?igsh=YnB1ZWNrZGtsdWQ=';
+    let linkedin = 'https://www.linkedin.com/in/jacopo-d-errico';
+
+    return {myEmail, gitHub, insta, linkedin}
+});

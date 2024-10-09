8ef53d60be39bd54cafa60510f5201905bdbc037
jacopo-derrico <derricoj.design@gmail.com>
2024-10-09 16:39:32 +0200

    feat: fetch data of single project to import in personalized page

---
+++ pages/[projects]/[slug].vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-primary">
+        {{ portfolioStore.singleProjectData.title }} titolo
+    </div>
+</template>
+
+<script setup>
+    import { usePortfolioStore } from '~/store/portfolio';
+
+    const portfolioStore = usePortfolioStore();
+
+    try {
+        portfolioStore.fetchSingleProject();
+    } catch (error) {
+        console.error('Error fetching project data:', error);
+    }
+
+    const projectData = portfolioStore.singleProjectData
+
+</script>
--- store/portfolio.ts
+++ store/portfolio.ts
@@ -5,18 +5,31 @@ import axios from 'axios';
 export const usePortfolioStore = defineStore('portfolioStore', {
     state: () => ({
         projects: [],
-        backUrl: ''
+        backUrl: '',
+        singleProject: null,
+        singleProjectData: []
     }),
     getters: {},
     actions: {
         async fetchProjects() {
             try {
+                this.singleProject = null;
                 const response = await axios.get('http://127.0.0.1:8000/api/projects');
                 this.projects = response.data.projects;
                 this.backUrl = response.data.baseUrl;
-                console.log(this.backUrl);
             } catch (error) {
                 console.error('Error fetching projects:', error);
+                throw error;
+            }
+        },
+        async fetchSingleProject() {
+            try {
+                const response = await axios.get(`http://127.0.0.1:8000/api/projects/${this.singleProject}`);
+                this.singleProjectData = response.data.project;
+                console.log(this.singleProjectData);
+                
+            } catch (error) {
+                console.error('Error fetching project data:', error);
                 throw error;
             }
         }

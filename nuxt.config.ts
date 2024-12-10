2488811d75943f4ed716c6e5de1362fb2e035ef2
jacopo-derrico <derricoj.design@gmail.com>
2024-12-10 15:30:15 +0100

    build: setting firebase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+  devServer: {
+    port: 8000
+  },
   modules: [
     '@nuxtjs/tailwindcss',
     '@nuxt/fonts',
@@ -33,6 +36,9 @@ export default defineNuxtConfig({
       messagingSenderId: "861651541162",
       appId: "1:861651541162:web:50e9d325bb8e6b262644d3",
       measurementId: "G-QYQ31BDYQZ"
+    },
+    services: {
+      firestore: true,
     },
     auth: {
       enabled: true

8cc07b2f8f4bf8c56e4f2f8c68bd90d3b36d36ad
jacopo-derrico <derricoj.design@gmail.com>
2024-12-11 23:32:42 +0100

    style: add icon to btn

--- components/HomeExperience.vue
+++ components/HomeExperience.vue
@@ -42,7 +42,12 @@
         </div>
         <div class="flex justify-center w-full mt-10">
             <a href="https://www.linkedin.com/in/jacopo-d-errico">
-                <button class="text-button uppercase btn-style py-3 px-5">_see more</button>
+                <button class="text-button uppercase btn-style py-3 px-5">_see more <svg
+            class="inline mb-[3px]" xmlns="http://www.w3.org/2000/svg" height="14px" viewBox="0 -960 960 960" width="14px"
+            fill="#01F0FF">
+            <path
+              d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h560v-280h80v280q0 33-23.5 56.5T760-120H200Zm188-212-56-56 372-372H560v-80h280v280h-80v-144L388-332Z" />
+          </svg></button>
             </a>
         </div>
     </section>

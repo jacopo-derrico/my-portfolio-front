9e37e88da24a7acf4189ade56ce6c3722964b68b
jacopo-derrico <derricoj.design@gmail.com>
2024-12-11 23:18:29 +0100

    feat: portfolio array

--- store/portfolio.ts
+++ store/portfolio.ts
@@ -24,7 +24,8 @@ export const usePortfolioStore = defineStore('portfolioStore', () => {
             description: "This tool has been created to give a preview of your website in Google's search engine. You can type in the title and the description and you will see the appearence on the side, with a bar that helps you understand if you are writing too much. In addition there are informations and links to dive deeper into SEO, if needed.",
             categories: ['Frontend', 'UX', 'UI'],
             technologies: ['Nuxt.js', 'Tailwind', 'GSAP', 'Figma'],
-            images: ['assets/proj/seo-preview-1.webp', 'assets/proj/seo-preview-2.webp', 'assets/proj/seo-preview-3.webp'],
+            cover: '/assets/proj/covers/seo-preview.webp',
+            images: ['/assets/proj/seo-preview-1.webp', '/assets/proj/seo-preview-2.webp', '/assets/proj/seo-preview-3.webp'],
             githubUrl: 'https://github.com/jacopo-derrico/SEO-preview',
             demoUrl: 'https://seo-preview.netlify.app/'
         },
@@ -40,20 +41,22 @@ export const usePortfolioStore = defineStore('portfolioStore', () => {
             description: "A website for a startup that works in the car repair field and connects repair shops and clients and help them pay the repair job through bank loans. It is simple in structure as it uses only HTML, CSS & JavaScript. The contact form uses Email.js to send emails.",
             categories: ['Frontend', 'UX', 'UI'],
             technologies: ['HTML', 'CSS', 'JavaScript', 'Figma'],
-            images: ['~/assets/proj/leox-1.webp', '~/assets/proj/leox-2.webp', '~/assets/proj/leox-3.webp', '~/assets/proj/leox-4.webp', '~/assets/proj/leox-5.webp', '~/assets/proj/leox-6.webp', '~/assets/proj/leox-7.webp'],
+            cover: '/assets/proj/covers/leox.webp',
+            images: ['/assets/proj/leox-1.webp', '/assets/proj/leox-2.webp', '/assets/proj/leox-3.webp', '/assets/proj/leox-4.webp', '/assets/proj/leox-5.webp', '/assets/proj/leox-6.webp', '/assets/proj/leox-7.webp'],
             githubUrl: 'https://github.com/jacopo-derrico/leox-solution-website',
             demoUrl: 'https://www.leoxsolution.com/'
         },
         {
             id: 3,
-            title: 'Startup rental website',
+            title: 'Startup rental website & logo',
             date: '2024',
             company: 'Omni Rent',
             collaborators: [],
-            description: "A company website for a startup that works in the rental field afor all kind of personal vehicles. It has been designed on Figma and developed in Nuxt.js and has optimized SEO. The animations are made with GSAP library and some components are created with the element UI module.",
+            description: "A company website for a startup that works in the rental field afor all kind of personal vehicles. It has been designed on Figma and developed in Nuxt.js and has optimized SEO. The animations are made with GSAP library and some components are created with the element UI module. The logo resembles the recycle logo, to hint to the ecology of renting.",
             categories: ['Frontend', 'UX', 'UI'],
-            technologies: ['Nuxt.js', 'GSAP', 'Tailwind', 'Figma'],
-            images: ['~/assets/proj/omni-1.webp'],
+            technologies: ['Nuxt.js', 'GSAP', 'Tailwind', 'Figma', 'Illustrator'],
+            cover: '/assets/proj/covers/',
+            images: ['/assets/proj/omni-1.webp'],
             githubUrl: 'https://github.com/jacopo-derrico/rent-website',
             demoUrl: ''
         },
@@ -66,9 +69,58 @@ export const usePortfolioStore = defineStore('portfolioStore', () => {
             description: "This is a test to create an e-commerce SEO-friendly, it buillds the sitemap based on an api call and has robot rules. It uses sitemap and robots modules for Nuxt",
             categories: ['Frontend'],
             technologies: ['Nuxt.js', 'Tailwind'],
-            images: ['~/assets/proj/ecommerce-1.webp', '~/assets/proj/ecommerce-2.webp', '~/assets/proj/ecommerce-3.webp', '~/assets/proj/ecommerce-4.webp', '~/assets/proj/ecommerce-5.webp'],
+            cover: '/assets/proj/covers/ecommerce.webp',
+            images: ['/assets/proj/ecommerce-1.webp', '/assets/proj/ecommerce-2.webp', '/assets/proj/ecommerce-3.webp', '/assets/proj/ecommerce-4.webp', '/assets/proj/ecommerce-5.webp'],
             githubUrl: 'https://github.com/jacopo-derrico/ecommerce-nuxt',
             demoUrl: 'https://e-commerce-ssr.netlify.app/'
+        },
+        {
+            id: 5,
+            title: 'Consultancy website & logo',
+            date: '2024',
+            company: 'Quality Effective Solutions',
+            collaborators: [{
+                name: 'Moni',
+                url: 'https://github.com/monica-harabagiu'
+            }],
+            description: "Consultancy website that works with startups to help them grow. The website is made on Wordpress with Elementor as a builder and some tweaks with CSS and JavaScript. The logo has the mascotte of the unicorn that can be used as an icon, colorful it has a gradient, but can easily be used in black or white.",
+            categories: ['Frontend', 'Backend', 'Logo'],
+            technologies: ['Wordpress', 'Elementor', 'Figma', 'Illustrator'],
+            cover: '/assets/proj/covers/qes.webp',
+            images: ['/assets/proj/qes-1.webp', '/assets/proj/qes-2.webp', '/assets/proj/qes-3.webp', '/assets/proj/qes-4.webp', '/assets/proj/qes-5.webp'],
+            githubUrl: '',
+            demoUrl: 'https://qesolutions.io/'
+        },
+        {
+            id: 6,
+            title: 'Photographer website',
+            date: '2023',
+            company: 'Andrea Lovera',
+            collaborators: [{
+                name: 'Moni',
+                url: 'https://github.com/monica-harabagiu'
+            }],
+            description: "A personal website for a photographer that helps him showcase his photos and virtual tour projects. It has different pages that shows specific kind of photos that has been optimized to not take too long on charging the content. The virtual tours are embedded from Kuula. Buttons have a special hover effect that resembles the camera settings.",
+            categories: ['Frontend', 'Backend'],
+            technologies: ['Wordpress', 'Spectra', 'Figma'],
+            cover: '/assets/proj/covers/lovera.webp',
+            images: ['/assets/proj/lovera-1.webp', '/assets/proj/lovera-2.webp', '/assets/proj/lovera-3.webp', '/assets/proj/lovera-4.webp', '/assets/proj/lovera-5.webp', '/assets/proj/lovera-6.webp', '/assets/proj/lovera-7.webp'],
+            githubUrl: '',
+            demoUrl: 'https://www.andrealovera.com/'
+        },
+        {
+            id: 7,
+            title: 'Nutritionist website',
+            date: '2023',
+            company: 'Lara Di Gennaro',
+            collaborators: [],
+            description: "A personal website for a nustritionist, it helps her explain what she does, what type of services she can give and talks a bit about herself and her education on the matter. It is clean and easy to navigate, a few animations help making it more dynamic.",
+            categories: ['Frontend', 'Backend'],
+            technologies: ['Wordpress', 'Elementor'],
+            cover: '/assets/proj/covers/digennaro.webp',
+            images: ['/assets/proj/digennaro-1.webp', '/assets/proj/digennaro-2.webp', '/assets/proj/digennaro-3.webp', '/assets/proj/digennaro-4.webp'],
+            githubUrl: '',
+            demoUrl: 'https://www.digennaronutrizionista.it/'
         },
     ]
 

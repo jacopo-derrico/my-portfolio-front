e28e6b19db2cc891ddcd874c65d69da05ab89245
jacopo-derrico <derricoj.design@gmail.com>
2024-12-12 12:22:07 +0100

    docs: personal description

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -3,7 +3,7 @@
         <div class="lg:w-1/2">
             <h3 class="text-primary">Jacopo D'Errico</h3>
             <h1 class="text-primary">Designer & Web Developer</h1>
-            <p class="text-secondary">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec quis mi placerat, elementum purus et, fermentum odio. Duis vitae nisi iaculis ante bibendum convallis in et massa. Quisque id mauris sit amet dui sagittis feugiat. Nulla ut lorem eros. Sed congue nisl lacus, quis tempus dolor condimentum at. Sed maximus, velit id suscipit suscipit, dui turpis pharetra eros, ultricies aliquam enim mi id eros. </p>
+            <p class="text-secondary">I have always had a passion for technology and design. During my undergraduate studies, I learned a design method and how to work in a team. I continued to explore the world of the web, starting with graphic design and then completing my journey with development.</p>
             <div class="flex justify-end mt-10">
                 <a href="">
                     <button class="text-button uppercase btn-style py-3 px-5">_see projects</button>

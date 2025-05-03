8ea7ec0d4e5e24ab1510172b587ed55c7c735357
jacopo-derrico <derricoj.design@gmail.com>
2025-05-03 11:55:47 +0200

    feat: create currentSection object with section number and name + tw  classes

--- components/AppSidebar.vue
+++ components/AppSidebar.vue
@@ -1,7 +1,7 @@
 <template>
-    <div id="sidebar" class="fixed secondary-font flex flex-col justify-center top-0 bottom-0 w-1/2 z-100">
+    <div id="sidebar" class="fixed secondary-font flex flex-col justify-center top-0 bottom-0 w-1/2 z-[100]">
         <div class="bars w-fit ps-6">
-            <span class="text-accent ms-[-7px]">01</span>
+            <span class="text-accent ms-[-7px]">{{ miscStore.currentSection.number }}</span>
             <div :class="{ 'active-bar': 'about-section' === activeId }" class="bar flex items-center gap-4">
                 <div class="bg-white vertical-bar h-[100px] w-[4px]"></div>
                 <NuxtLink to="#about-section">_about</NuxtLink>
@@ -37,10 +37,15 @@
         observer.value = new IntersectionObserver(
             (entries) => {
             entries.forEach(entry => {
-                const id = entry.target.getAttribute('id')
+                const id = entry.target.getAttribute('id');
+                const sectionNumber = entry.target.getAttribute('data-section-number');
                 if (entry.isIntersecting && id) {
-                    activeId.value = id
-                    miscStore.currentSection = id.split('-')[0]
+                    activeId.value = id;
+                    miscStore.currentSection =  {
+                        sectionName: id.split('-')[0],
+                        number: sectionNumber
+                    };
+                    console.log(miscStore.currentSection);
                 }
             })
             },
@@ -51,13 +56,13 @@
         )
 
         document.querySelectorAll('#about-section, #experience-section, #portfolio-section, #contacts-section').forEach((section) => {
-            observer.value?.observe(section)
+            observer.value?.observe(section);
         })
     })
 
     onUnmounted(() => {
         if (observer.value) {
-            observer.value.disconnect()
+            observer.value.disconnect();
         }
     })
 </script>

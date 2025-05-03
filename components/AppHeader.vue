13e1dddd077ac881b3fe2f373d81f77fdfcd8774
jacopo-derrico <derricoj.design@gmail.com>
2025-05-03 12:11:59 +0200

    fix: current section name, z-index and hide ling placeholder

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,12 +1,12 @@
 <template>
-    <header class="fixed top-0 w-full">
-        <div class="flex justify-between items-center my-8 mx-auto py-10 lg:py-0 lg:w-10/12">
-            <span class="text-primary">
-                _lang
+    <header class="fixed top-0 w-full z-50">
+        <div class="flex justify-between items-center my-4 mx-auto py-10 lg:py-0 lg:w-10/12">
+            <span class="text-primary w-1/4">
+                <!-- _lang -->
             </span>
-            <AppLogo />
-            <span class="text-accent">
-                _{{ miscStore.currentSection }}
+            <AppLogo class="w-1/2"/>
+            <span class="text-accent w-1/4 text-end">
+                _{{ miscStore.currentSection.sectionName }}
             </span>
         </div>
     </header>

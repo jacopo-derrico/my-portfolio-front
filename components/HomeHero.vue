fa78a98bfe12b2621e555045bddca8f95736f39e
jacopo-derrico <derricoj.design@gmail.com>
2025-05-03 11:56:33 +0200

    feat: add data attribute for section number

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -1,5 +1,5 @@
 <template>
-    <section id="about-section" class="flex mx-auto py-10 my-[200px] lg:py-0 lg:w-10/12">
+    <section id="about-section" data-section-number="01" class="flex mx-auto py-10 my-[200px] lg:py-0 lg:w-10/12">
         <div class="lg:w-1/2">
             <h3 class="text-primary">Jacopo D'Errico</h3>
             <h1 class="text-primary">Designer & Web Developer</h1>

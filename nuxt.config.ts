d53ee3966b2ad7111c173b981267a93b729fc7db
jacopo-derrico <derricoj.design@gmail.com>
2025-05-05 18:06:42 +0200

    feat: setup locale config and json files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -27,21 +27,26 @@ export default defineNuxtConfig({
   fonts: {
     local: {},
   },
-  // vuefire: {
-  //   config: {
-  //     apiKey: "AIzaSyDq855EBxLYYtHwwDlsECP5QrJ6lu9o6OM",
-  //     authDomain: "my-portfolio-34fff.firebaseapp.com",
-  //     projectId: "my-portfolio-34fff",
-  //     storageBucket: "my-portfolio-34fff.appspot.com",
-  //     messagingSenderId: "861651541162",
-  //     appId: "1:861651541162:web:50e9d325bb8e6b262644d3",
-  //     measurementId: "G-QYQ31BDYQZ"
-  //   },
-  //   services: {
-  //     firestore: true,
-  //   },
-  //   auth: {
-  //     enabled: true
-  //   },
-  // },
+  i18n: {
+    lazy: true,
+    langDir: '/locales',
+    strategy: 'prefix',
+    locales: [
+      { 
+        code: 'en', 
+        language: 'en-US',
+        iso: "en-US", 
+        name: '_english',
+        file: 'en-US.json'
+      },
+      { 
+        code: 'it', 
+        language: 'it-IT',
+        iso: "it-IT",
+        name: '_italiano',
+        file: 'it-IT.json'
+      }
+    ],
+    defaultLocale: 'en',
+  }
 })

828be304923782094aa5b4094e131ddb49860555
jacopo-derrico <derricoj.design@gmail.com>
2025-05-05 18:10:52 +0200

    feat: add language switcher to header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -2,7 +2,10 @@
     <header class="fixed top-0 w-full z-50">
         <div class="flex justify-between items-center my-4 mx-auto py-10 lg:py-0 lg:w-10/12">
             <span class="text-primary w-1/4">
-                <!-- _lang -->
+                <!-- <a href="#" v-for="locale in availableLocales" :key="locale.code" @click.prevent.stop="setLocale(locale.code)">
+                    {{ locale.name }}
+                </a> -->
+                <LangSwitcher />
             </span>
             <AppLogo class="w-1/2"/>
             <span class="text-accent w-1/4 text-end">
@@ -16,4 +19,11 @@
     import { useMiscStore } from '~/store/misc';
 
     const miscStore = useMiscStore();
+
+    // const { locale, locales } = useI18n()
+    // const switchLocalePath = useSwitchLocalePath()
+
+    // const availableLocales = computed(() => {
+    //     return locales.value.filter(i => i.code !== locale.value)
+    // })
 </script>
---
+++ components/LangSwitcher.vue
@@ -0,0 +1,23 @@
+<script setup>
+const { locales, locale } = useI18n();
+const switchLocalePath = useSwitchLocalePath()
+
+const availableLocales = computed(() => {
+  return locales.value.filter(i => i.code !== locale.value)
+})
+</script>
+
+<template>
+    <div class="language-switcher">
+        <!-- Current language -->
+        <div class="current-language" >
+          _{{ $i18n.locale }}
+          <!-- Dropdown list -->
+          <div class="language-dropdown">
+            <NuxtLink v-for="locale in availableLocales" :key="locale.code" :to="switchLocalePath(locale.code)">
+              _{{ locale.code }}
+            </NuxtLink>
+          </div>
+        </div>
+    </div>
+</template>

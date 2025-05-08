557df84f583dba9c3785dc594838329373624192
jacopo-derrico <derricoj.design@gmail.com>
2025-05-08 17:27:06 +0200

    fix: project path with localePath

--- components/HomePortfolio.vue
+++ components/HomePortfolio.vue
@@ -23,7 +23,7 @@
                 </div>
             </NuxtLink> -->
 
-            <NuxtLink v-for="project in portfolio.projects" :to="`/projects/${project.id}`" :key="project.id" class="relative hover-info" :onclick="portfolio.singleProject = project.id">
+            <NuxtLink v-for="project in portfolio.projects" :to="localePath(`/projects/${project.id}`)" :key="project.id" class="relative hover-info" :onclick="portfolio.singleProject = project.id">
                 <img class="w-full h-full rounded-xl shadow"
                     :src="`${project.images[0]}`" alt="">
                 <div class="absolute bottom-0 left-0 top-0 right-0 bg-black bg-opacity-80 flex flex-col-reverse p-4 hidden-info">
@@ -47,6 +47,7 @@
 </template>
 
 <script setup>
+    const localePath = useLocalePath()
     import { usePortfolioStore } from '~/store/portfolio';
 
     const portfolio = usePortfolioStore();

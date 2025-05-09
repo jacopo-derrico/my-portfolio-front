df3dc158c12f68b92d1440b93daa45a1c85d6986
jacopo-derrico <derricoj.design@gmail.com>
2025-05-09 18:37:35 +0200

    fix: project page link misplaced

--- components/HomePortfolio.vue
+++ components/HomePortfolio.vue
@@ -2,8 +2,8 @@
     <section id="portfolio-section" data-section-number="03" class="mx-auto py-10 my-[200px] lg:py-0 lg:w-10/12">
         <h3 class="text-button mb-10">// portfolio</h3>
         <div class="columns-2 md:columns-3 gap-4 space-y-4">
-            <div v-for="project in portfolio.projects" :to="`/projects/${project.slug}`" :key="project.id">
-                <NuxtLink class="relative hover-info d-block mb-4" :onclick="portfolio.singleProject = project.id">
+            <div v-for="project in portfolio.projects" :key="project.id">
+                <NuxtLink class="relative hover-info d-block mb-4" :to="`/projects/${project.slug}`" :onclick="portfolio.singleProject = project.id">
                     <img class="w-full h-full rounded-xl shadow"
                         :src="`${project.images[0]}`" alt="">
                     <div class="absolute bottom-0 left-0 top-0 right-0 bg-black bg-opacity-80 flex flex-col-reverse p-4 hidden-info">

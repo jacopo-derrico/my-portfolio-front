605d7fa07fa6494b60000e9541592dca52a8527b
jacopo-derrico <derricoj.design@gmail.com>
2025-05-14 17:38:22 +0200

    chore: removed comment

--- components/HomePortfolio.vue
+++ components/HomePortfolio.vue
@@ -4,8 +4,6 @@
         <div class="columns-2 lg:columns-3 gap-4 space-y-4">
             <div v-for="project in portfolio.projects" :key="project.id">
                 <NuxtLink class="relative hover-info d-block mb-4" :to="`/projects/${project.slug}`" :onclick="portfolio.singleProject = project.id">
-                    <!-- <img class="w-full h-full rounded-xl shadow"
-                        :src="`${project.images[0]}`" :alt="`${project.title} - cover image`"> -->
                         <NuxtImg :src="project.images[0]" :alt="`${project.title} - cover image`" class="w-full h-full rounded-xl shadow" sizes="sm:180px md:350px lg:240px" loading="lazy" format="webp" />
                     <div class="absolute bottom-0 left-0 top-0 right-0 bg-black bg-opacity-80 flex flex-col-reverse p-2 md:p-4 hidden-info">
                         <div class="hidden md:flex gap-2 flex-wrap">

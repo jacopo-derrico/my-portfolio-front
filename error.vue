c09b215b5c5f59039d45759851695f98392deb0c
jacopo-derrico <derricoj.design@gmail.com>
2025-05-21 17:34:53 +0200

    feat: add error page for SEO (min style)

---
+++ error.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+
+// Set proper status code
+definePageMeta({
+  layout: 'error'
+})
+
+useHead({
+  title: `Error ${props.error.statusCode || '500'}`,
+  meta: [
+    { name: 'robots', content: 'noindex' }
+  ]
+})
+</script>
+
+<template>
+  <div id="error-page" class="w-full h-[100vh] flex flex-col justify-center align-items-center">
+    <div class="my-auto text-center">
+      <h1 class="text-9xl mb-5">{{ error.statusCode }}</h1>
+      <p v-if="error.statusMessage" class="mb-32">{{ error.statusMessage }}</p>
+      <a href="/">
+        <button class="text-button uppercase btn-style py-3 px-5">
+          _go back home
+        </button>
+      </a>
+    </div>
+  </div>
+</template>

ae27aa572c5a7418a9e5a11c7be44f86f1b64f58
jacopo-derrico <derricoj.design@gmail.com>
2025-12-17 11:15:03 +0100

    style: fix overlays on different browsers

--- components/HomePortfolio.vue
+++ components/HomePortfolio.vue
@@ -3,9 +3,9 @@
         <h3 class="text-button mb-10">// portfolio</h3>
         <div class="columns-2 lg:columns-3 gap-4 space-y-4">
             <div v-for="project in portfolio.projects" :key="project.id">
-                <NuxtLink class="relative hover-info d-block mb-4" :to="`/projects/${project.slug}`" :onclick="portfolio.singleProject = project.id">
+                <NuxtLink class="relative hover-info block mb-4 overflow-hidden rounded-xl" :to="`/projects/${project.slug}`" :onclick="portfolio.singleProject = project.id">
                     <NuxtImg :src="project.images[0]" :alt="`${project.title} - cover image`" class="w-full h-full rounded-xl shadow" sizes="sm:180px md:350px lg:240px" loading="lazy" format="webp" />
-                    <div class="absolute bottom-0 left-0 top-0 right-0 bg-black bg-opacity-80 flex flex-col-reverse p-2 md:p-4 hidden-info">
+                    <div class="absolute inset-0 bg-black bg-opacity-80 flex flex-col-reverse p-2 md:p-4 hidden-info rounded-xl">
                         <div class="hidden md:flex gap-2 flex-wrap">
                             <span v-for="(tag) in project.technologies" :key="tag.indexOf" class="technology pill-type">
                                 {{tag}}
